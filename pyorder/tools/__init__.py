a8fd895682a10f2a94e5924dfa0219254842822b
Dominique Orban <orban@chimay.cerfacs.fr>
2010-08-19 17:44:24 +0200

    Skip 'spy' gracefully if Matplotlib is not present.

--- pyorder/tools/__init__.py
+++ pyorder/tools/__init__.py
@@ -3,7 +3,13 @@ Various tools to complement PyOrder.
 """
 
 from hrb import *
-from spy import *
+
+try:
+    from spy import *
+except:
+    sys.stderr.write('Warning: Matplotlib is not installed. ')
+    sys.stderr.write('No plots will be produced.\n')
+
 from conversion import *
 
 __all__ = filter(lambda s:not s.startswith('_'), dir())
